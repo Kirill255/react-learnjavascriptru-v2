6cb0b150ed86df230100764b744ce6489ffadf1e
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-12 17:42:51 +0300

    article list component

---
+++ src/components/article-list.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Article from "./article";
+
+class ArticleList extends Component {
+  // constructor(...args) {
+  //   super(...args);
+
+  //   this.state = {
+  //     articleId: null
+  //   };
+  // }
+
+  state = {
+    articleId: null
+  };
+
+  toggleOpenArticle = (articleId) => (event) => this.setState({ articleId });
+
+  get body() {
+    return this.props.articles.map((article) => (
+      <li key={article.id}>
+        <Article
+          article={article}
+          isOpen={this.state.articleId === article.id}
+          toggleOpen={this.toggleOpenArticle(article.id)}
+        />
+      </li>
+    ));
+  }
+
+  render() {
+    return <ul>{this.body}</ul>;
+  }
+}
+
+export default ArticleList;

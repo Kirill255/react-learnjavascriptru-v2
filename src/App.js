29ba76e46db2c8960da250350e49117ecc08ab4a
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-12 20:17:19 +0300

    add react-select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import { findDOMNode } from "react-dom";
+import Select from "react-select";
 import ArticleList from "./components/article-list";
 import ArticlesChart from "./components/articles-chart";
 import articles from "./fixtures";
 
 class App extends Component {
+  state = {
+    openItem: null
+  };
+
+  get options() {
+    return articles.map((article) => ({
+      label: article.title,
+      value: article.id
+    }));
+  }
+
+  handleSelect = (openItem) => this.setState({ openItem });
+
   setArticleListRef = (ref) => {
     console.log(ref);
     console.log(findDOMNode(ref));
@@ -18,6 +32,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Select options={this.options} value={this.state.openItem} onChange={this.handleSelect} />
         <ArticleList ref={this.setArticleListRef} articles={articles} />
         <ArticlesChart articles={articles} />
       </div>

bb1f0c269ba488c04c6d200e4f27403c41a1700e
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-13 09:30:40 +0300

    componentDidCatch

--- src/components/article.js
+++ src/components/article.js
@@ -2,11 +2,25 @@ import React, { PureComponent } from "react";
 import CommentList from "./comment-list";
 
 class Article extends PureComponent {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch(err) {
+    // отлавливает ошибки ниже по иерархии, тоесть ошибки которые случились в childrens, в нашем случае в CommentList, поэтому стоит перехватывать ошибки на самом верхнем уровне, в App
+    // в dev режиме всё ровно будет отображаться overlay с ошибкой, но его можно закрыть, в prod режиме всё будет нормально, без overlay
+    console.log("---", err);
+    this.setState({
+      hasError: true
+    });
+  }
+
   handleClick = () => this.props.toggleOpen(this.props.article.id);
 
   get body() {
     const { isOpen, article } = this.props;
     if (!isOpen) return null;
+    if (this.state.hasError) return <div>Some Error in this article</div>;
 
     return (
       <section>

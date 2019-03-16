61ce60bfc87be32d1c478006ec6b1d0096a2438c
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-16 15:08:00 +0300

    add router as a prop

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -42,12 +42,11 @@ export default connect(
   (state) => {
     return {
       articles: filtratedArticles(state),
-      loading: articlesLoadingSelector(state)
+      loading: articlesLoadingSelector(state),
+      router: state.router
     };
   },
-  { fetchData: loadAllArticles },
-  null,
-  { pure: false }
+  { fetchData: loadAllArticles }
 )(ArticleList);
 
 /*
@@ -58,4 +57,10 @@ import {ArticleList} from "./article-list"
 
 export default accordion(ArticleList) - экспортируем класс завёрнутый в декоратор
 import ArticleList from "./article-list" // сдесь имя сами придумываем, поэтому если импортируем в один файл, чтобы небыло конфликта можем назвать например ArticleListWithAccordion
+*/
+
+/*
+connect под капотом использует shouldComponentUpdate() ну или PureComponent, чтобы отключить это поведение можно в connect 4'ым параметром передать {pure: false}
+
+также shouldComponentUpdate() ну или PureComponent работает по принципу изменения пропсов, если пропсы не изменились, то компонент не будет перерендерен, сообственно ещё один способ, можно сказать "грязный хак", это передать в пропсы наш роутер, тоесть если изменися роутер, значит изменились пропсы, плюсы нам не нужно нужно в каждом компоненте выставлять {pure: false}, а вложенность может быть большой, минусы это требует подключения "connected-react-router", ведь мы читаем роутер из redux'а, а роутер появляется в redux именно из-за "connected-react-router"
 */
